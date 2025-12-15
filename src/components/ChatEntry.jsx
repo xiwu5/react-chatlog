b2dcac56171c1c7c78f44d95703add399201e157
Xi <youngjoycewu@gmail.com>
2025-12-16 05:21:21 +0800

    refactor 'props' for readability

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -2,26 +2,23 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
-  let entryClass = 'remote';
-  if (props.isLocal) {
-    entryClass = 'local';
-  }
+const ChatEntry = ({ id, sender, body, timeStamp, isLocal, liked, onUpdateLiked }) => {
+  const entryClass = isLocal ? 'local' : 'remote';
 
   const handleLikeClick = () => {
-    if (props.onUpdateLiked) {
-      props.onUpdateLiked(props.id);
+    if (onUpdateLiked) {
+      onUpdateLiked(id);
     }
   };
 
   return (
     <article className={`chat-entry ${entryClass}`}>
-      <h2 className="entry-name">{props.sender}</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>{props.body}</p>
-        <p className="entry-time"><TimeStamp time={props.timeStamp} /></p>
+        <p>{body}</p>
+        <p className="entry-time"><TimeStamp time={timeStamp} /></p>
         <button className="like" onClick={handleLikeClick}>
-          {props.liked ? '❤️' : '🤍'}
+          {liked ? '❤️' : '🤍'}
         </button>
       </section>
     </article>
